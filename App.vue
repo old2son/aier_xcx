c9512a47af586173b93dc74f812cc4739716af36
caiye <caiye2531889700@google.com>
2026-05-08 10:36:58 +0800

    🌈 style(无指定范围): 代码缩进

--- App.vue
+++ App.vue
@@ -1,79 +1,79 @@
 <script>
-  export default {
-    onLaunch: function() {
-      uni.hideTabBar()
-      // 微信小程序原生Tab监听
-      // 统一监听所有页面显示（包括tab页和非tab页）
-      uni.onAppRoute(async (res) => {
-        const path = res.path.startsWith('/') ? res.path : '/' + res.path
-        // 判断是否允许跳转
-        const allowed = await this.$store.dispatch('moduleTab/checkTabNeedLogin', path)
-        if (!allowed) {
-          uni.reLaunch({ url: '/subpackages/packageLogin/login' })
-          return
-        }
-        // 正常设置 tab 状态
-        this.$store.commit('moduleTab/SET_CURRENT_TAB', path)
-      })
-      // 微信小程序专属tab监听（双保险）
-      // #ifdef MP-WEIXIN
-      if (typeof uni.onTabItemTap === 'function') {
-        uni.onTabItemTap((item) => {
-          const path = item.pagePath.startsWith('/') ? item.pagePath : `/${item.pagePath}`
-          this.$store.commit('moduleTab/SET_CURRENT_TAB', path)
-        })
-      }
-      // #endif
-    },
-    onShow: function() {
-      // console.log('App Show')
-    },
-    onHide: function() {
-      // console.log('App Hide')
-    }
-  }
+export default {
+	onLaunch: function () {
+		uni.hideTabBar();
+		// 微信小程序原生Tab监听
+		// 统一监听所有页面显示（包括tab页和非tab页）
+		uni.onAppRoute(async (res) => {
+			const path = res.path.startsWith('/') ? res.path : '/' + res.path;
+			// 判断是否允许跳转
+			const allowed = await this.$store.dispatch('moduleTab/checkTabNeedLogin', path);
+			if (!allowed) {
+				uni.reLaunch({ url: '/subpackages/packageLogin/login' });
+				return;
+			}
+			// 正常设置 tab 状态
+			this.$store.commit('moduleTab/SET_CURRENT_TAB', path);
+		});
+		// 微信小程序专属tab监听（双保险）
+		// #ifdef MP-WEIXIN
+		if (typeof uni.onTabItemTap === 'function') {
+			uni.onTabItemTap((item) => {
+				const path = item.pagePath.startsWith('/') ? item.pagePath : `/${item.pagePath}`;
+				this.$store.commit('moduleTab/SET_CURRENT_TAB', path);
+			});
+		}
+		// #endif
+	},
+	onShow: function () {
+		// console.log('App Show')
+	},
+	onHide: function () {
+		// console.log('App Hide')
+	}
+};
 </script>
 
 <style lang="scss">
-  /* vant-weapp样式 */
-  @import "wxcomponents/vant/common/index.wxss";
-  /*每个页面公共css */
-  @import '@/uni_modules/uni-scss/index.scss';
-  /* #ifndef APP-NVUE */
-  @import '@/static/customicons.css';
-  // 设置整个项目的背景色
-  page {
-    width: 100%;
-    height: 100%;
-    overflow-y: auto;
-    background-color: #f9f9f9;
-  }
-  .van-checkbox {
-    display: flex;
-    align-items: stretch !important;
-  }
-  image {
-    font-size: 0;
-    vertical-align: middle;
-  }
-  
-  .empty-box {
-    width: 100%;
-    height: 65vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    .empty-image {
-      width: 120px;
-      height: 120px;
-    }
-    .empty-text {
-      margin-top: 60rpx;
-      color: #BBBBBB;
-      font-size: 24rpx;
-      font-weight: 550;
-    }
-  }
-  /* #endif */
+/* vant-weapp样式 */
+@import 'wxcomponents/vant/common/index.wxss';
+/*每个页面公共css */
+@import '@/uni_modules/uni-scss/index.scss';
+/* #ifndef APP-NVUE */
+@import '@/static/customicons.css';
+// 设置整个项目的背景色
+page {
+	width: 100%;
+	height: 100%;
+	overflow-y: auto;
+	background-color: #f9f9f9;
+}
+.van-checkbox {
+	display: flex;
+	align-items: stretch !important;
+}
+image {
+	font-size: 0;
+	vertical-align: middle;
+}
+
+.empty-box {
+	width: 100%;
+	height: 65vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	.empty-image {
+		width: 120px;
+		height: 120px;
+	}
+	.empty-text {
+		margin-top: 60rpx;
+		color: #bbbbbb;
+		font-size: 24rpx;
+		font-weight: 550;
+	}
+}
+/* #endif */
 </style>
--- subpackages/packageAppointment/appointmentEntrance.vue
+++ subpackages/packageAppointment/appointmentEntrance.vue
@@ -1,139 +1,139 @@
 <template>
-  <view class="appointment-entrance-container">
-    <view class="logo-box">
-      <image :src="appointmentData" mode="widthFix"></image>
-    </view>
-    <view class="appintment-card">
-      <view class="back">
-        <image class="back-image" :src="appintCardBack"/>
-       <view class="appintment-entrance-wrap">
-          <view class="per-appt" @click="toSubpackagePage('/subpackages/packageAppointment/personalAppointment')">
-            <image :src="perImage" mode="widthFix"></image>
-          </view>
-          <view class="team-appt" @click="toSubpackagePage('/subpackages/packageAppointment/teamAppointment')">
-            <image :src="teamImage" mode="widthFix"></image>
-          </view>
-        </view>
-        <view class="appintment-guide">
-          <text>温馨提示：预约前请先阅读
-            <text class="link-guide" @click="toSubpackagePage('/subpackages/packageCategory/visitKnow/index?active=1')">《参观须知》</text>
-          </text>
-        </view>
-      </view>
-
-    </view>
-
-  </view>
+	<view class="appointment-entrance-container">
+		<view class="logo-box">
+			<image :src="appointmentData" mode="widthFix"></image>
+		</view>
+		<view class="appintment-card">
+			<view class="back">
+				<image class="back-image" :src="appintCardBack" />
+				<view class="appintment-entrance-wrap">
+					<view class="per-appt" @click="toSubpackagePage('/subpackages/packageAppointment/personalAppointment')">
+						<image :src="perImage" mode="widthFix"></image>
+					</view>
+					<view class="team-appt" @click="toSubpackagePage('/subpackages/packageAppointment/teamAppointment')">
+						<image :src="teamImage" mode="widthFix"></image>
+					</view>
+				</view>
+				<view class="appintment-guide">
+					<text>
+						温馨提示：预约前请先阅读
+						<text class="link-guide" @click="toSubpackagePage('/subpackages/packageCategory/visitKnow/index?active=1')">《参观须知》</text>
+					</text>
+				</view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-import appointmentData from '@/data/appointment.json'
-import loginData from '@/data/login.json'
+import appointmentData from '@/data/appointment.json';
+import loginData from '@/data/login.json';
 
 export default {
-  data() {
-    return {
-      appointmentData: appointmentData[0].appointmentLogo,
-      appintCardBack: appointmentData[0].appintCardBack,
-      perImage: appointmentData[0].perImage,
-      teamImage: appointmentData[0].teamImage
-    };
-  },
-  onLoad() {
-    // this.loadH5Url()
-    console.log('...',this.appointmentData)
-  },
-  methods: {
-    toSubpackagePage (url) {
-      uni.navigateTo({
-        url: url
-      })
-    },
-  }
-}
+	data() {
+		return {
+			appointmentData: appointmentData[0].appointmentLogo,
+			appintCardBack: appointmentData[0].appintCardBack,
+			perImage: appointmentData[0].perImage,
+			teamImage: appointmentData[0].teamImage
+		};
+	},
+	onLoad() {
+		// this.loadH5Url()
+		console.log('...', this.appointmentData);
+	},
+	methods: {
+		toSubpackagePage(url) {
+			uni.navigateTo({
+				url: url
+			});
+		}
+	}
+};
 </script>
 
 <style lang="scss" scoped>
 .appointment-entrance-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 100%;
-  height: 100vh;
-  background-image: url(https://geducloud0617.oss-cn-shenzhen.aliyuncs.com/aier-applet/%E9%A2%84%E7%BA%A6%E5%85%A5%E5%8F%A3%E8%83%8C%E6%99%AF%E5%9B%BE.png);
-  background-size: cover;
-  
-  .logo-box {
-    width: 100%;
-    height: 35vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    image {
-      width: 188px;
-      height: 125px;
-    }
-  }
-  
-  .appintment-card {
-    width: 80%;
-    height: 65vh;
-    margin: 0 auto;
-    .back {
-      position: relative;
-      width: 100%;
-      height: 100%;
-      .back-image {
-        position: absolute;
-        top: 0;
-        width: 100%;
-        height: 65%;
-      }
-      .appintment-entrance-wrap {
-        position: absolute;
-        width: 100%;
-        height: 65%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        .per-appt,.team-appt {
-          width: 80%;
-          text-align: center;
-          image {
-            width: 220px;
-            height: 78px
-          }
-        }
-        .per-appt {
-          margin-top: 5vh;
-        }
-        .team-appt {
-          margin-top: 2vh;
-        }
-      }
-    }
-  }
-  
-  .appintment-guide {
-    position: absolute;
-    bottom: 20%;
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: #0766ff;
-    border-radius: 40rpx;
-    color: #fff;
-    font-size: 24rpx;
-    padding: 12rpx 24rpx;
-    box-sizing: border-box;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    white-space: nowrap;
-    .link-guide {
-      text-decoration: underline;
-      color: #fff; /* 可选：像链接颜色 */
-    }
-  }
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 100%;
+	height: 100vh;
+	background-image: url(https://geducloud0617.oss-cn-shenzhen.aliyuncs.com/aier-applet/%E9%A2%84%E7%BA%A6%E5%85%A5%E5%8F%A3%E8%83%8C%E6%99%AF%E5%9B%BE.png);
+	background-size: cover;
+
+	.logo-box {
+		width: 100%;
+		height: 35vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		image {
+			width: 188px;
+			height: 125px;
+		}
+	}
+
+	.appintment-card {
+		width: 80%;
+		height: 65vh;
+		margin: 0 auto;
+		.back {
+			position: relative;
+			width: 100%;
+			height: 100%;
+			.back-image {
+				position: absolute;
+				top: 0;
+				width: 100%;
+				height: 65%;
+			}
+			.appintment-entrance-wrap {
+				position: absolute;
+				width: 100%;
+				height: 65%;
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				.per-appt,
+				.team-appt {
+					width: 80%;
+					text-align: center;
+					image {
+						width: 220px;
+						height: 78px;
+					}
+				}
+				.per-appt {
+					margin-top: 5vh;
+				}
+				.team-appt {
+					margin-top: 2vh;
+				}
+			}
+		}
+	}
+
+	.appintment-guide {
+		position: absolute;
+		bottom: 20%;
+		left: 50%;
+		transform: translateX(-50%);
+		background-color: #0766ff;
+		border-radius: 40rpx;
+		color: #fff;
+		font-size: 24rpx;
+		padding: 12rpx 24rpx;
+		box-sizing: border-box;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		white-space: nowrap;
+		.link-guide {
+			text-decoration: underline;
+			color: #fff; /* 可选：像链接颜色 */
+		}
+	}
 }
 </style>
